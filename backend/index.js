028fae5de7f2fb7c8df34ba4bfc2059da133e7ea
Vitor <v188344@dac.unicamp.br>
2021-05-26 22:23:03 -0300

    Código no Heroku

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,69 @@
-//console.log("Hello World and Vitor");
-const express = require('express');
+const express = require("express");
 const app = express();
+app.use(express.json());
 
-app.get('/', function(req, res){
-    res.send('Hello World and Tafs');
-})
-app.listen(process.env.PORT||3000);
+//Permissões
+ var cors = require('cors')
+ app.use(cors());
+
+ //Porta que estou ouvindo
+
+ app.listen(process.env.PORT || 3000);
+
+ app.get('/', function (req, res){
+        res.send("Hello World and Tafs");
+     }
+ ); 
+
+ app.get('/hello', function (req, res){
+        res.send("Olá professor!");
+     }
+ );
+
+ const mensagens = [
+    "Vitor Cintra", "Rafa Yoko"
+ ];
+
+ app.get('/mensagens', function(req, res){
+    // res.send(mensagens);
+     res.send(mensagens.filter(Boolean));
+    }
+);
+
+app.get('/mensagens/:id', function(req, res)
+    {
+        const id = req.params.id - 1;
+        const mensagem = mensagens[id];
+        
+        if(!mensagem){
+            res.send('Mensagem não encontrada');
+        } else {
+            res.send(mensagem);
+        }
+    }
+);
+app.post('/mensagens',
+        function(req, res){
+            console.log(req.body.mensagem);
+            const mensagem = req.body.mensagem ;
+            mensagens.push(mensagem);
+            res.send("criar uma mensagem");
+        }
+    );
+
+    app.put('/mensagens/:id',
+     (req, res) => {
+        const id = req.params.id - 1;
+        const mensagem = req.body.mensagem;
+        mensagens[id] = mensagem;
+        res.send("Mensagem atualizada com sucesso")  
+    }
+);
+
+app.delete('/mensagens/:id',
+        (req, res) => {
+           const id = req.params.id -1;
+           delete mensagens[id];
+           res.send("Mensagem removida com sucesso")
+        }
+    );
